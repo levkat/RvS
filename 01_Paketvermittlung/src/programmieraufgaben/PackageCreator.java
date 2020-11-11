8b9ae452fb4ed88708826d0f9adab708d8c716e4
lev <katsevlev@gmail.com>
2020-11-11 18:54:05 +0100

    add
    add files

---
+++ 01_Paketvermittlung/src/programmieraufgaben/PackageCreator.java
@@ -0,0 +1,42 @@
+package programmieraufgaben;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PackageCreator {
+
+    /**
+     * Hier sollen die Kommandozeilen-Abfragen abgefragt und die Antworten
+     * gespeichert werden
+     * Es sollte auf Fehlerbehandlung geachtet werden (falsche Eingaben, ...)
+     *
+     * @param dataPackage Hier wird das Objekt übergeben in das die abgefragten Werte gespeichert werden sollen
+     * @return Gibt das als Parameter übergebene Objekt, dass mit den abgefragten Werten befüllt wurde zurück
+     */
+    public DataPackage fillParameters(DataPackage dataPackage) {
+
+        return dataPackage;
+    }
+
+    /**
+     * Aus dem als Parameter übergebenen Paket sollen die Informationen
+     * ausgelesen und in einzelne Datenpakete aufgeteilt werden
+     *
+     * @param dataPackage Hier wird das Objekt übergeben in das das Resultat gespeichert werden soll
+     * @return Gibt das als Parameter übergebene Objekt mit den aufgeteiltet Datenpaketen zurück
+     */
+    public List<DataPackage> splitPackage(DataPackage dataPackage) {
+        List<DataPackage> dataPackages = new LinkedList<>();
+
+        return dataPackages;
+    }
+
+    /**
+     * Diese Methode gibt den Inhalt der empfangenen Pakete in der Komandozeile aus
+     *
+     * @param dataPackages Hier wird die Liste übergeben, deren Elemente in die Kommandozeile ausgegeben werden sollen
+     */
+    public void printOutPackage(List<DataPackage> dataPackages) {
+
+    }
+}
